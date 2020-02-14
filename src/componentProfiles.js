5a98cf1e93d222da9de59841201847f7d3715bde
Tim Vlasuk <timvlasuk@gmail.com>
2020-02-14 12:37:42 -0800

    added a mock data file to cycle through mock components for generating a menu list and putting operational components into functioning links

---
+++ src/componentProfiles.js
@@ -0,0 +1,35 @@
+const availablePaths = [
+  {
+    name: 'Clock',
+    filePath: '/src/components/clock.js',
+    publicPath: '/clock',
+    operational: true
+  },
+  {
+    name: 'Source-Drain',
+    filePath: '',
+    publicPath: '',
+    operational: false
+  },
+  {
+    name: 'DragNDrop',
+    filePath: '',
+    publicPath: '',
+    operational: false
+  },
+  {
+    name: 'UrbanLegends',
+    filePath: '',
+    publicPath: '',
+    operational: false
+  },
+  {
+    name: 'CloverStocks',
+    filePath: '',
+    publicPath: '',
+    operational: false
+  },
+  
+]
+
+export default availablePaths
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,35 @@
 /* */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import availablePaths from '../componentProfiles';
+
+
 
 export default function Navigate() {
 
-  const clock = {
-    name: 'clock',
-    path: '/clock'
-  }
-
-  console.log(clock)
+  const menuItems = availablePaths.map((component, index) => 
+    component.operational ? <li key={index}>
+      <Link to={component.publicPath}>{component.name}</Link>
+    </li> :
+    <li key={index}>
+      {component.name}
+    </li>
+  )
 
   return (
     <div style={{border: 'solid 2px gold'}} id="component-navigation">
         <ul>
-          <li><Link to={clock.name}>{clock.name}</Link></li>
+          {menuItems}
         </ul>
     </div>
   )
 }
+
+
+/*
+  "Keys only make sense in the context of the surrounding array" - 
+    if you wrap the <li> into a different component/element, the key should be transfered to the new element as long as it's still in the array
+  Keys need to be unique among siblings, though different lists/arrays can share key values
+
+
+    */
